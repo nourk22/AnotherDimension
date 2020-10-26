7e4e3c9f2fbf4b82fec862ca8e554ecc5ec7956c
Nour <ckastoun20@stuy.edu>
2020-10-25 23:37:13 -0400

    tested 4b and it works

--- Tester.java
+++ Tester.java
@@ -1,21 +1,22 @@
 public class Tester{
   public static void main(String[] args) {
     //testing 4a
-    int[] a = {1, 2, 3, 4, 5};
-    int[] b = {2, 4, 6, 8};
-    int[] c = {100, 150, 200};
+    int[] a = {5, 2, 3, 4, 5};
+    int[] b = {2, 4, 8, 6};
+    int[] c = {100, 150, 150, 200};
     int[] d = {};
-    System.out.println(ArrayOps.sum(a)); //expecting 15
+    System.out.println(ArrayOps.sum(a)); //expecting 19
     System.out.println(ArrayOps.sum(b)); //expecting 20
-    System.out.println(ArrayOps.sum(c)); //expecting 450
+    System.out.println(ArrayOps.sum(c)); //expecting 600
     System.out.println(ArrayOps.sum(d)); //expecting 0
-    //
-  /*  System.out.println(ArrayOps.
-    System.out.println(ArrayOps.
-    System.out.println(ArrayOps.
+
+    //testing 4b
+    System.out.println(ArrayOps.largest(a)); //expecting 5
+    System.out.println(ArrayOps.largest(b)); //expecting 8
+    System.out.println(ArrayOps.largest(c)); //expecting 200
 
     //
-    System.out.println(ArrayOps.
+  /*  System.out.println(ArrayOps.
     System.out.println(ArrayOps.
     System.out.println(ArrayOps.
     System.out.println(ArrayOps.
