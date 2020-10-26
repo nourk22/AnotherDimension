6cec4e24e60b194c15123299e6d65ff0e346e97a
Nour <ckastoun20@stuy.edu>
2020-10-25 23:28:23 -0400

    tested 4a for ArrayOps and it works

---
+++ Tester.java
@@ -0,0 +1,28 @@
+public class Tester{
+  public static void main(String[] args) {
+    //testing 4a
+    int[] a = {1, 2, 3, 4, 5};
+    int[] b = {2, 4, 6, 8};
+    int[] c = {100, 150, 200};
+    System.out.println(ArrayOps.sum(a)); //expecting 15
+    System.out.println(ArrayOps.sum(b)); //expecting 20
+    System.out.println(ArrayOps.sum(c)); //expecting 450
+    //
+  /*  System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+
+    //
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+
+    //
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    System.out.println(ArrayOps.
+    */
+  }
+}
