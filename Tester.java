fee29b6eae78907e68c622c6644dc8bdf91f733f
Nour <ckastoun20@stuy.edu>
2020-10-25 23:31:36 -0400

    tested 4a for empty array and it works

--- Tester.java
+++ Tester.java
@@ -4,9 +4,11 @@ public class Tester{
     int[] a = {1, 2, 3, 4, 5};
     int[] b = {2, 4, 6, 8};
     int[] c = {100, 150, 200};
+    int[] d = {};
     System.out.println(ArrayOps.sum(a)); //expecting 15
     System.out.println(ArrayOps.sum(b)); //expecting 20
     System.out.println(ArrayOps.sum(c)); //expecting 450
+    System.out.println(ArrayOps.sum(d)); //expecting 0
     //
   /*  System.out.println(ArrayOps.
     System.out.println(ArrayOps.
