9573400582ab05c8c8a7603a9cf712e883ed1297
Nour <ckastoun20@stuy.edu>
2020-10-25 23:13:07 -0400

    4a done, no compile errors, not yet tested

---
+++ ArrayOps.java
@@ -0,0 +1,47 @@
+public class ArrayOps{
+  //4a
+  public static int sum(int[] arr){
+    int sum = 0;
+    for (int i=0; i<arr.length; i++){
+      sum+=arr[i];
+    }
+    return sum;
+  }
+
+  /*public static int largest(int[] arr){
+
+  }
+
+  public static int[] sumRows(int[][] matrix){
+
+  }
+
+  public static int largestInRows(int[][] matrix){
+
+  }
+
+  public static int sum(int[][] arr){
+
+  }
+
+  public static int sumCols(int[][] matrix){
+
+  }
+
+  public static boolean isRowMagic(int[][] matrix){
+
+  }
+
+  public static boolean isColMagic(int[][] matrix){
+
+  }
+
+  public static boolean isLocationMagic(int[][] matrix, int row, int col){
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+  */
+}
